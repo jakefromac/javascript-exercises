226b8b794fc508577167ecc49c3a2ac26228f482
Jake Logan <109786756+jakefromac@users.noreply.github.com>
2023-01-16 16:43:18 -0800

    Solution for 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,9 @@
-const repeatString = function() {
-
+const repeatString = function(whatToRepeat, repeatLength) {
+    let repeatedThings = "";
+    for (let i = 0; i < repeatLength; i++) {
+        repeatedThings += whatToRepeat;
+    }
+    return repeatedThings;
 };
 
 // Do not edit below this line
