6234ee291da8fbb482fb2cf20769e48dba7bd1fc
Jake Logan <109786756+jakefromac@users.noreply.github.com>
2023-01-17 07:46:57 -0800

    Solution for 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,8 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    var strSplit = str.split('');
+    strSplit.reverse();
+    let strReversed = strSplit.join('');
+    return strReversed;
 };
 
 // Do not edit below this line
