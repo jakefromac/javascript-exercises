d4b376581fc3b75c8fab6f6e311d1593600feb16
Jake Logan <109786756+jakefromac@users.noreply.github.com>
2023-01-30 21:42:14 -0800

    Solution for 06.

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,6 +1,18 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    return isLeapYear(year);
 };
+
+function isLeapYear(year) {
+    if (!isInteger(year)) { return "ERROR"; }
+
+    return ((year % 4 == 0) && (year % 100 != 0)) || (year % 400 == 0);
+}
+
+function isInteger(checkNumber) {
+    return typeof(checkNumber) === 'number' &&
+            isFinite(checkNumber) &&
+            Math.round(checkNumber) === checkNumber;
+}
 
 // Do not edit below this line
 module.exports = leapYears;
